b70317267cf13fb1045646216d7db652d8b1d2c7
geallenboy <gejialun88@163.com>
2021-03-09 15:33:07 +0800

    allen提交

--- basics/crypto/Hmac.js
+++ basics/crypto/Hmac.js
@@ -1,1 +1,7 @@
 'use strict';
+let crypto = require('crypto');
+
+const hmac = crypto.createHmac('sha256', 'geallen');
+hmac.update('hello,world!');
+hmac.update('hello,nodejs');
+console.log(hmac.digest('hex'));
